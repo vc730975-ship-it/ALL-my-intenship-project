a88f4947809132a48c49388fde3200742a426b99
kallu chaudhary <vc730975@gmail.com>
2025-12-02 15:40:52 +0530

    Create Ranges.py

---
+++ Ranges.py
@@ -0,0 +1,29 @@
+# for i in range(5):
+#     print("1")
+
+
+#Reverse Range from 10 to 0 
+
+# for i in range(10,-1,-1):
+#  print(i)
+
+#Ranges Practise
+# for i in range(5):
+#     print("i love python")
+
+    # square root 
+# for i in range (0,10):
+#     print(f"square of no. {i}=",i*i)
+
+    #pattern printing 
+
+# for i in range(6,0,-1):
+    
+#         print(i* "*")
+
+#Reverse pattern printing 
+for i in range(1,6,):
+    
+        print(i* "*")
+
+        
