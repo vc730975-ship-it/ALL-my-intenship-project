91113d8bb98f78744abf7650cc5f7308b41af843
kallu chaudhary <vc730975@gmail.com>
2025-12-02 15:39:25 +0530

    Create Movies.py

---
+++ Movies.py
@@ -0,0 +1,10 @@
+age = int (input("Enter your age :") )
+ticket = True
+if age > 18 :         
+    print ("You can watch the movie")
+if age > 18:
+        print("Firstly Show Your Id Card then go")
+elif age < 15:
+    print ("Your can watch the movie with parents")
+else : 
+    print("Not Allowed")
